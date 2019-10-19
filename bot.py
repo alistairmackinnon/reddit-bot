a1244dd338d89916d80931655f146430e14acfd0
Alistair <alistair.mackinnon@outlook.com>
2019-10-19 21:17:48 +0100

    comment summary single comment

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@
 import config
 import praw
 from praw.models import MoreComments
+import re
 
 reddit = praw.Reddit(   client_id = 'UgPvUahzEm-iPQ'
                         , client_secret = '-TLSHC-A46k2OvdVl3Educaksck'
@@ -12,13 +13,35 @@ reddit = praw.Reddit(   client_id = 'UgPvUahzEm-iPQ'
                         , password = config.reddit_password)
 
 print(reddit.user.me())
-details = {}
+opinion_check = re.compile(r'YTA|NTA|ESH|NAH')
 
-submissions = reddit.subreddit('amitheasshole').top(limit = 1)
+submissions = reddit.subreddit('amitheasshole').hot(limit = 1)
+comment_details = {}
+
+comment_details['id'] = ''
+comment_details['yta_cnt'] = 0
+comment_details['nta_cnt'] = 0
+comment_details['esh_cnt'] = 0
+comment_details['nah_cnt'] = 0
 
 for submission in submissions:
     comments = submission.comments
     for top_level_comment in comments:
         if isinstance(top_level_comment, MoreComments):
             continue
-        print(top_level_comment.body)
+        opinion = opinion_check.search(top_level_comment.body)
+        if opinion:
+            match_value = opinion.group()
+            comment_details['id'] = top_level_comment.id
+            if match_value == 'YTA':
+                comment_details['yta_cnt'] += 1
+            elif match_value == 'NTA':
+                comment_details['nta_cnt'] += 1
+            elif match_value == 'ESH':
+                comment_details['esh_cnt'] += 1
+            elif match_value == 'NAH':
+                comment_details['nah_cnt'] += 1
+    print(comment_details)
+
+
+
