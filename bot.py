c4b928841b39ffd11149e53f360d41e30ece3ec6
Alistair <alistair.mackinnon@outlook.com>
2019-10-19 21:57:46 +0100

    summarising columns

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import config
 import praw
 from praw.models import MoreComments
 import re
+import pprint as pp
 
 reddit = praw.Reddit(   client_id = 'UgPvUahzEm-iPQ'
                         , client_secret = '-TLSHC-A46k2OvdVl3Educaksck'
@@ -15,33 +16,42 @@ reddit = praw.Reddit(   client_id = 'UgPvUahzEm-iPQ'
 print(reddit.user.me())
 opinion_check = re.compile(r'YTA|NTA|ESH|NAH')
 
-submissions = reddit.subreddit('amitheasshole').hot(limit = 1)
-comment_details = {}
+submissions = reddit.subreddit('amitheasshole').hot(limit = 10)
 
-comment_details['id'] = ''
-comment_details['yta_cnt'] = 0
-comment_details['nta_cnt'] = 0
-comment_details['esh_cnt'] = 0
-comment_details['nah_cnt'] = 0
+
+
+posts = {}
 
 for submission in submissions:
     comments = submission.comments
+    submission_id = submission.id
+    post_details = {}
+    post_details['yta_cnt'] = 0
+    post_details['nta_cnt'] = 0
+    post_details['esh_cnt'] = 0
+    post_details['nah_cnt'] = 0
+
     for top_level_comment in comments:
         if isinstance(top_level_comment, MoreComments):
             continue
         opinion = opinion_check.search(top_level_comment.body)
         if opinion:
             match_value = opinion.group()
-            comment_details['id'] = top_level_comment.id
             if match_value == 'YTA':
-                comment_details['yta_cnt'] += 1
+                post_details['yta_cnt'] += 1
             elif match_value == 'NTA':
-                comment_details['nta_cnt'] += 1
+                post_details['nta_cnt'] += 1
             elif match_value == 'ESH':
-                comment_details['esh_cnt'] += 1
+                post_details['esh_cnt'] += 1
             elif match_value == 'NAH':
-                comment_details['nah_cnt'] += 1
-    print(comment_details)
+                post_details['nah_cnt'] += 1
+
+
+
+    posts[submission_id] = post_details
+pp.pprint(posts)
+
+
 
 
 
