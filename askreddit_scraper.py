ed0ea704e5945109bdc744057d371cf890f1eb03
Ali <alistair.mackinnon@outlook.com>
2019-11-25 23:27:12 +0000

    initial ask reddit scraper

---
+++ askreddit_scraper.py
@@ -0,0 +1,35 @@
+# Bot will get replies to the comment and determine asshole or not
+
+
+import config
+import praw
+from praw.models import MoreComments
+
+reddit = praw.Reddit(client_id=config.client_id
+                     , client_secret=config.client_secret
+                     , user_agent='testscript by /u/fakebot3'
+                     , username=config.reddit_username
+                     , password=config.reddit_password)
+
+posts = {}
+
+for submission in reddit.subreddit('askreddit').controversial(limit=10):
+    sub = reddit.submission(id=submission)
+    posts[sub.id] = {'title': sub.title, 'ratio': sub.upvote_ratio, 'score': sub.score, 'num_comments': sub.num_comments}
+
+for i in posts:
+    print(posts[i]['title'])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
