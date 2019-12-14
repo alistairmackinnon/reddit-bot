512cba6c6c17e148b5bc8b8ebfe518e890723173
Ali <alistair.mackinnon@outlook.com>
2019-12-14 22:24:31 +0000

    median score banded

--- askreddit_scraper.py
+++ askreddit_scraper.py
@@ -14,10 +14,10 @@ reddit = praw.Reddit(client_id=config.client_id
                      , password=config.reddit_password)
 
 comments = []
-for submission in reddit.subreddit('askreddit').hot(limit=1):
+for submission in reddit.subreddit('askreddit').hot(limit=5):
     print(submission.title)
     sub = reddit.submission(id=submission)
-    submission.comments.replace_more(limit=10)
+    submission.comments.replace_more(limit=15)
     submission.comment_sort = 'top'
     post_time = submission.created_utc
 
@@ -25,13 +25,33 @@ for submission in reddit.subreddit('askreddit').hot(limit=1):
     for top_level_comment in submission.comments:
         sec_since_post = top_level_comment.created_utc - post_time
         min_since_post = round(sec_since_post / 60)
-        details = (top_level_comment.id,  min_since_post, top_level_comment.score)
+        if min_since_post <= 5:
+            band = '01 - 5 mins'
+        elif min_since_post <= 15:
+            band = '02 - 15 mins'
+        elif min_since_post <= 30:
+            band = '03 -30 mins'
+        elif min_since_post <= 45:
+            band = '04 - 45 mins'
+        elif min_since_post <= 60:
+            band = '05 - 60 mins'
+        elif min_since_post <= 90:
+            band = '06 - 90 mins'
+        elif min_since_post <= 90:
+            band = '07 - 120 mins'
+        elif min_since_post <= 90:
+            band = '08 - 180 mins'
+        else:
+            continue
+        details = (top_level_comment.id,  band, top_level_comment.score)
         comments.append(details)
 
-df = pd.DataFrame.from_records(comments, columns=['id', 'min_since_post', 'score'])
+df = pd.DataFrame.from_records(comments, columns=['id', 'band', 'score'])
 
-df2 = df.groupby('min_since_post').mean().rolling(5)
+df2 = df.groupby('band').median()
 df2.sort_index(inplace=True)
+
+print(df2['score'])
 
 pltx = df2.plot(figsize=(10,4), legend=False)
 plt.show(pltx)
